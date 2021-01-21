9291c55236b746132082c95a08f3b75a655adda7
yonatan zonszein <yonatanzonszein@gmail.com>
2021-01-21 17:10:45 -0500

    Initialized deck card number and suit

--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -1,3 +1,20 @@
 require "rspec"
 require "deck.rb" 
 
+describe Deck do
+    
+    describe "#intialize" do
+    let(:new_deck) {Deck.new}
+    it "should have 52 cards" do 
+        expect(deck.cards.count).to eq(52)
+    end
+    it "should be 50% black cards and 50% red cards" do
+        expect(deck.cards.count{|card|card.color == black}.to eq(26)
+    it "it should be 25% of each suit" do
+
+
+    end
+
+end
+
+
